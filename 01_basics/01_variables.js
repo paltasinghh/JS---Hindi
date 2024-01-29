9d514f5f743be7031cbc5e7a8dd27241fe7a0c80
paltasinghh <111628214+paltasinghh@users.noreply.github.com>
2024-01-29 11:22:01 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "paltasinghh@gmail.com"
+var accountPassword = "12087"
+accountCity = "Bhubaneswar"
+let accountState;
+//accountId = 2 // not allowed
+/*Prefor not to use Var
+ because of issue in block scope & functional Scope
+*/
+accountEmail = "himasu123@gmail.com"
+accountPassword = "512443"
+accountCity = "Cuttack"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+//alert (3 + 3) //we are using node js not browser
+
+console.log( 3003+736734) //code readability should be high
+ console.log("Himansu")
+ let age=23
+
+ // number => 20 to the power 53
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+
+
+ console.log(typeof undefined) // undefined
+ console.log(typeof null) // object
