b28a2861d3df62c8f4dce37fd3d49ef437637ca9
paltasinghh <111628214+paltasinghh@users.noreply.github.com>
2024-01-31 20:20:43 +0530

    Create 03_conversionOperation.js

---
+++ 03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "123abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = "Himansu"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false
+// "" => false
+// "Himansu" =>true
+
+let somenumber = 33
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
