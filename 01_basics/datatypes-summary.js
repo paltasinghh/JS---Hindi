539c646c80121c67f617f7b80a623e366b4a5eaa
paltasinghh <111628214+paltasinghh@users.noreply.github.com>
2024-03-18 06:27:29 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+const id = Symbol('123')
+const anotherId = Sysmbol('143')
+const heros = ["himanus", "paltasingh"];
+let myObj =  {
+    name: "Himansu",
+    age: 23,
+}
+const myFunction = function()
+{
+    console.log("Hello Himansu");
+}
+// console.log(typeof id);
+// console.log(typeof anotherId);
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof id);
+console.log(typeof id);
