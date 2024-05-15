d80d798f250859fbd51ee504845e2988b056c4b1
paltasinghh <111628214+paltasinghh@users.noreply.github.com>
2024-05-15 22:04:44 +0530

    Create 06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score=400
+//console.log(score)
+const balance =new Number(100) 
+//console.log(balance)
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(2))
+const otherNumber = 23.8966
+//console.log(otherNumber.toPrecision(5));
+const hundred =10000000
+//console.log(hundred.toLocaleString())
+
+// ### Math ###
+
+console.log(Math);
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.9))
+// console.log(Math.max(4,5,3,7,))
+// console.log(Math.min(0,3,4,5))
+// console.log(Math.random())
+// console.log(Math.pow(19,9))
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+const min=10
+const max=30
+console.log(Math.random()*(max-min+1))
+console.log(Math.floor(Math.random()*(max-min+1)))
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
